6971c3568fb6c2421226f593d4c8b316780bb956
Jake Mack <jmackin5@mail.naz.edu>
2025-12-10 08:51:04 -0500

    cleanup

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+# # Needed for older version of MacOS to avoid MPS backend usage in PyTorch
+# import os
+# # remove fallback variable and try to force CPU usage
+# os.environ.pop("PYTORCH_ENABLE_MPS_FALLBACK", None)
+# os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "0"
+
+# # import torch and try to disable MPS backend flags before docling imports it
+# import torch
+# torch.backends.mps.is_available = lambda : False
+# torch.backends.mps.is_built = lambda : False
+# try:
+#     torch.set_default_device("cpu")
+# except Exception:
+#     pass
+
+
 from thrive_ai import ThriveAiDocling
 
 docling = ThriveAiDocling()
 
-processing_docs = False 
+processing_docs = True 
 
 if processing_docs: 
     online_context = docling.process_pdf("https://arxiv.org/pdf/2408.09869")
